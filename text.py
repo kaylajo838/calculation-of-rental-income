13407850bc3cbf5e0ffd5d7ce43d56cf8c10b2f0
Kayla Imming <kaylaimming@gmail.com>
2023-02-17 18:17:22 -0800

    Add code to calculate ROI on rental investments

---
+++ text.py
@@ -0,0 +1,40 @@
+# 4 square method
+
+# 1. Income
+#   - rental income - 2000
+#   - laundry - 0
+#   - storage - 0
+#   - misc - 0
+#   - Total monthly income = $2000
+
+# 2.Expenses
+# Tax - 150
+# Insurace - 100
+# Utilities - 0
+#   - electric
+#   - water
+#   - sewer
+#   - garbage
+#   - gas
+# HOA - 0
+# Lawn/Snow - 0
+# Vacancy - 100
+# Repairs - 100
+# CapEx - 100
+# Property Management - 200
+# Mortgage - 860
+# Total Expenses = $1610
+
+# 3. Cash flow
+# income - expenses
+# Total monthly cash flow = $390
+
+#4. Cash on Cash ROI
+# Down Payment - 40,000
+# Closing costs - 3,000
+# Rehab budget - $7,000
+# misc - 0
+# Total investment - $ 50,000
+# Anual cash flow -  $4,680
+# ROI = Annual Cash flow / total investment
+# Cash on Cash ROI - 9.36%
